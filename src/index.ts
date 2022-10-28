aa0c2e87e07690ff3bfa16bbc6485c3a535bc0f0
tongzhiyang <tongzhiyang@souche.com>
2022-10-28 16:45:37 +0800

    feat: export default babelPlugin

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ import babelPlugin from "./babel-plugin";
 import vitePlugin from './vite-plugin'
 
 export type { ParseOption } from './config'
-export { babelPlugin, vitePlugin } 
+export { babelPlugin, vitePlugin }
+export default babelPlugin
